8cf93fec0687ed53176d6420a50a6b0b11ceb04f
N308-ui <bardahamza24@gmail.com>
2025-08-19 09:15:02 +0100

    building containers

--- srcs/requirements/nginx/tools/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Generate SSL certificates if they don't exist
-if [ ! -f "/etc/ssl/certs/nginx.crt" ]; then
-    ./generate_certs.sh
-fi
-
-# Start NGINX
-exec "$@"
--- srcs/requirements/nginx/tools/generate_certs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p /etc/ssl/private /etc/ssl/certs
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/nginx.key \
-    -out /etc/ssl/certs/nginx.crt \
-    -subj "/C=FR/ST=Paris/L=Paris/O=42School/OU=hbarda/CN=hbarda.42.fr"
